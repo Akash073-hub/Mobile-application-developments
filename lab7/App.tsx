606010ffc4e822d319d4089825f9ba6163579fca
akash bhat <akashramachandrabhatbca24@rvu.edu.in>
2026-02-19 11:06:44 +0530

    Update App.tsx

--- lab7/App.tsx
+++ lab7/App.tsx
@@ -1,1 +1,44 @@
 
+import React from "react";
+import { FlatList,StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import data from './data.json'
+import { Text } from "react-native";
+import { View } from "react-native";
+import { StyleSheet } from "react-native";
+
+function App() {
+  return (
+    <SafeAreaView>
+      <FlatList
+      data={data}
+      renderItem = {({ item }) => (
+        <View style = {styles.card}>
+        <Text style = {styles.text}>{item.name}</Text>
+        <Text style = {styles.text}>{item.type}</Text>
+        </View>
+      )
+    }
+      />
+    </SafeAreaView>
+  );
+}
+
+export default App;
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor :'white',
+    borderRadius :8,
+    borderWidth :1,
+  },
+  card:{
+    padding:10,
+    backgroundColor:'grey',
+    borderRadius :8,
+    borderWidth :1,
+  },
+  text:{
+    fontSize:18,
+  }
+});
